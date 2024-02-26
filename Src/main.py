be1b9642e0cd0db8fb9527f9a3892d4f43f50450
Brenno Nogueira Alves <brenno1231@outlook.com>
2024-02-26 17:40:51 -0300

    1.0

--- Src/main.py
+++ Src/main.py
@@ -48,8 +48,6 @@ def carregar():
     df_imoveis = df_imoveis.replace('', np.nan).fillna(np.nan).dropna(subset=["cidade"])
     df_imoveis["cidade"] = df_imoveis["cidade"].astype("str")
 
-    df_imoveis = df_imoveis.head(1000)
-
     return df_imoveis, cidades_canonicas, modelo
 
 def normalizar_cidades(df_imoveis, cidades_canonicas, modelo):
